9678a9f9788fb28d506f36f549ffc399a11aee08
RuslanUC <dev_ruslan_uc@protonmail.com>
2025-07-11 18:09:47 +0300

    register secret chats handler

--- piltover/app/handlers/messages/__init__.py
+++ piltover/app/handlers/messages/__init__.py
@@ -1,5 +1,5 @@
 from piltover.worker import MessageHandler
-from . import stubs, sending, history, dialogs, other, chats, reactions, invites, saved_dialogs, polls, folders
+from . import stubs, sending, history, dialogs, other, chats, reactions, invites, saved_dialogs, polls, folders, secret
 
 handler = MessageHandler("messages")
 handler.register_handler(stubs.handler)
@@ -13,3 +13,4 @@ handler.register_handler(invites.handler)
 handler.register_handler(saved_dialogs.handler)
 handler.register_handler(polls.handler)
 handler.register_handler(folders.handler)
+handler.register_handler(secret.handler)
