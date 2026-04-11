77d03eeb347ab827eb4f422cdcccd8fe9c7d65f3
RuslanUC <dev_ruslan_uc@protonmail.com>
2026-04-11 20:15:00 +0300

    set seq and qts after NeedsContextValues instance check

--- piltover/session/session.py
+++ piltover/session/session.py
@@ -134,19 +134,23 @@ class Session:
 
                 del parent[int(field_path[-1])]
 
-        # TODO: use *ToFormat
-        if isinstance(obj, Updates) and self.auth_id is not None:
-            if (auth := await UserAuthorization.get_or_none(id=self.auth_id)) is not None:
-                auth.upd_seq += 1
-                await auth.save(update_fields=["upd_seq"])
-                obj.seq = auth.upd_seq
-                obj.qts = auth.upd_qts
-
         context_values = None
         if isinstance(obj, NeedsContextValues):
             with measure_time("._resolve_context_values(...)"):
                 context_values = await self._resolve_context_values(obj)
             obj = obj.obj
+
+        # TODO: use *ToFormat?
+        if isinstance(obj, Updates) and self.auth_id is not None:
+            await UserAuthorization.filter(id=self.auth_id).update(upd_seq=F("upd_seq") + 1)
+            seq_and_qts = await UserAuthorization.get_or_none(id=self.auth_id).values_list("upd_seq", "upd_qts")
+            if seq_and_qts is None:
+                upd_seq = upd_qts = 0
+            else:
+                upd_seq, upd_qts = seq_and_qts
+            logger.trace(f"setting seq to {upd_seq} for user {self.user_id}, auth {self.auth_id}")
+            obj.seq = upd_seq
+            obj.qts = upd_qts
 
         with measure_time("session.pack_message(...)"):
             message = self.pack_message(obj, in_reply)
