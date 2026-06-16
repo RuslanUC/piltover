f7d44cfd2141b7e69c1e7fb7a4e8218ace6b3878
RuslanUC <dev_ruslan_uc@protonmail.com>
2026-06-16 18:50:07 +0300

    fetch seq and qts in GetState by auth id instead of key id

--- piltover/app/handlers/updates.py
+++ piltover/app/handlers/updates.py
@@ -27,16 +27,10 @@ CHANNEL_UPDATES_TIMEOUT = 10
 
 async def get_seq_qts() -> tuple[int, int]:
     ctx = request_ctx.get()
-    seq_qts = await UserAuthorization.filter(
-        key_id=ctx.perm_auth_key_id,
-    ).first().values_list("upd_seq", "upd_qts")
-    if seq_qts is None:  # pragma: no cover
-        logger.warning(
-            f"Somehow auth is None for key {ctx.auth_key_id} (perm {ctx.perm_auth_key_id}), "
-            f"but it is in get_state_internal, where authorization must exist ???"
-        )
-
-    return seq_qts or (0, 0)
+    return cast(
+        tuple[int, int],
+        await UserAuthorization.filter(id=ctx.auth_id).first().values_list("upd_seq", "upd_qts")
+    )
 
 
 async def get_state_internal(user_id: int, pts: int | None = None) -> TLState:
