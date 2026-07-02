1b2ce677cdf2c990d3d0e6c1c1760393cee32872
RuslanUC <dev_ruslan_uc@protonmail.com>
2026-07-02 16:55:42 +0300

    rewrite unread count calculation in ReadState.get_in_out_ids_and_unread_bulk with raw sql query

--- piltover/db/models/read_state.py
+++ piltover/db/models/read_state.py
@@ -1,13 +1,23 @@
 from __future__ import annotations
 
-from tortoise import fields, Model
-from tortoise.expressions import Q
+from pypika_tortoise import Dialects, Parameter
+from tortoise import fields, Model, Tortoise
 from tortoise.functions import Count
 from tortoise.transactions import in_transaction
 
 from piltover.db import models
 from piltover.db.enums import PeerType
 from piltover.exceptions import Unreachable
+
+_UNREAD_COUNTS_SQL = """
+SELECT
+    state.peer_id peer, COUNT(mref.id) count
+FROM readstate state
+    JOIN messageref mref on state.peer_id = mref.peer_id and mref.id > state.last_message_id
+WHERE state.owner_id = {user_id_param} AND state.peer_id {peer_condition}
+GROUP BY state.peer_id
+;
+"""
 
 
 class ReadState(Model):
@@ -50,14 +60,31 @@ class ReadState(Model):
 
         in_read_states = await cls.for_peers_bulk(user_id, peers)
 
-        unreads_queries = [
-            Q(peer=peer, id__gt=in_read_state.last_message_id)
-            for peer, in_read_state in zip(peers, in_read_states)
-        ]
-        unread_counts = await models.MessageRef.filter(
-            Q(*unreads_queries, join_type=Q.OR),
-        ).group_by("peer_id").annotate(count=Count("id")).values_list("peer_id", "count")
-        unread_by_peer: dict[int, int] = dict(unread_counts)
+        fetch_unreads_for = []
+        for peer, read_state in zip(peers, in_read_states):
+            if (peer.last_message_id or 0) > read_state.last_message_id:
+                fetch_unreads_for.append(peer.id)
+
+        unread_by_peer = {}
+        if fetch_unreads_for:
+            conn = Tortoise.get_connection("default")
+            dialect = Dialects(conn.capabilities.dialect)
+            placeholder_factory = Parameter.IDX_PLACEHOLDERS[dialect]
+            placeholders = [placeholder_factory(i + 1) for i in range(len(peers) + 1)]
+
+            if len(peers) == 1:
+                where_condition = f"= {placeholders[1]}"
+            else:
+                where_condition = f"IN ({','.join(placeholders[1:])})"
+
+            sql = _UNREAD_COUNTS_SQL.format(user_id_param=placeholders[0], peer_condition=where_condition)
+            params = [user_id]
+            for peer in peers:
+                params.append(peer.id)
+
+            _, results = await conn.execute_query(sql, params)
+            for res in results:
+                unread_by_peer[res["peer"]] = res["count"]
 
         unread_reactions_by_peer = {}
         if not no_reactions:
@@ -77,6 +104,9 @@ class ReadState(Model):
         if not no_mentions:
             unread_target_ids = set()
             for peer in peers:
+                if peer.id not in unread_by_peer:
+                    # If no new messages - there can't be new mentions
+                    continue
                 if peer.type is PeerType.CHANNEL:
                     unread_target_ids.add(models.Channel.make_id_from(peer.channel_id))
                 elif peer.type is PeerType.CHAT:
