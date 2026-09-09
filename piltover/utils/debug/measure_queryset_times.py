2a1a8e0880cd859b3731dc75c6f2547db78274bd
RuslanUC <dev_ruslan_uc@protonmail.com>
2026-09-09 17:39:36 +0300

    add more classes/methods to measure_queryset_times.py

--- piltover/utils/debug/measure_queryset_times.py
+++ piltover/utils/debug/measure_queryset_times.py
@@ -5,6 +5,9 @@ from typing import Any
 from collections.abc import Iterable, Callable
 
 from loguru import logger
+from tortoise import BaseDBAsyncClient
+from tortoise.backends.base.client import PoolConnectionWrapper
+from tortoise.backends.base.executor import BaseExecutor
 from tortoise.queryset import BulkCreateQuery, BulkUpdateQuery, RawSQLQuery, ValuesQuery, ValuesListQuery, \
     CountQuery, ExistsQuery, DeleteQuery, UpdateQuery, QuerySet, AwaitableQuery
 
@@ -12,6 +15,13 @@ try:
     from tortoise.queryset_compiled import CompiledQuerySet
 except ImportError:
     CompiledQuerySet = None
+
+from tortoise.backends.sqlite import SqliteClient
+
+try:
+    from tortoise.backends.mysql import MySQLClient
+except ImportError:
+    MySQLClient = None
 
 from piltover.session import Session
 from piltover.utils.debug import measure_time
@@ -23,12 +33,22 @@ query_clss: list[type[AwaitableQuery]] = [
 ]
 if CompiledQuerySet is not None:
     query_clss.append(CompiledQuerySet)
+client_clss: list[type[BaseDBAsyncClient]] = [
+    SqliteClient,
+]
+if MySQLClient is not None:
+    client_clss.append(MySQLClient)
 execute_methods = ("execute", "_execute_many", "_execute",)
 make_query_methods = ("_get_or_create_cached_sql", "_make_queries", "_make_query",)
 call_methods = ("__call__",)
 resolve_ctx_methods = ("_resolve_context_values",)
+executor_select_methods = ("execute_select",)
+client_methods = ("execute_insert", "execute_query", "execute_script", "execute_many")
+aenter_methods = ("__aenter__",)
+aexit_methods = ("__aexit__",)
 real_suffix = "_real"
 handler_stats_ctx: ContextVar[QueryStats] = ContextVar("handler_stats_ctx")
+_using_executor_ctx: ContextVar[bool] = ContextVar("_using_executor_ctx", default=False)
 
 
 class QueryStats:
@@ -37,18 +57,30 @@ class QueryStats:
         self.make_query_time = 0
         self.execute_count = 0
         self.execute_time = 0
+        self.executor_count = 0
+        self.executor_time = 0
+        self.client_count = 0
+        self.client_time = 0
 
     def reset(self) -> None:
         self.make_query_count = 0
         self.make_query_time = 0
         self.execute_count = 0
         self.execute_time = 0
+        self.executor_count = 0
+        self.executor_time = 0
+        self.client_count = 0
+        self.client_time = 0
 
     def add(self, stats: QueryStats) -> None:
         self.make_query_count += stats.make_query_count
         self.make_query_time += stats.make_query_time
         self.execute_count += stats.execute_count
         self.execute_time += stats.execute_time
+        self.executor_count += stats.executor_count
+        self.executor_time += stats.executor_time
+        self.client_count += stats.client_count
+        self.client_time += stats.client_time
 
 
 def _patch_cls_replace_method(cls: type, names: Iterable[str], suffix: str, replace_with: Callable) -> None:
@@ -118,6 +150,24 @@ def patch_queryset_for_measurement() -> QueryStats:
 
     _patch_cls_replace_method(Session, resolve_ctx_methods, real_suffix, _Session__resolve_context_values)
 
+    async def _BaseExecutor_execute_select(self: BaseExecutor, *args, **kwargs):
+        name, execute_real = _get_patched_cls_original_method(self, executor_select_methods, real_suffix)
+        token = _using_executor_ctx.set(True)
+        try:
+            with measure_time(f"{self.__class__.__name__}.{name}()", add_depth=3) as _time_spent:
+                result = await execute_real(*args, **kwargs)
+        finally:
+            _using_executor_ctx.reset(token)
+
+        query_stats = handler_stats_ctx.get(None)
+        if query_stats is not None:
+            query_stats.executor_count += 1
+            query_stats.executor_time += _time_spent.ms
+
+        return result
+
+    _patch_cls_replace_method(BaseExecutor, executor_select_methods, real_suffix, _BaseExecutor_execute_select)
+
     for cls in query_clss:
         async def _execute(self: AwaitableQuery, *args, **kwargs) -> Any:
             name, execute_real = _get_patched_cls_original_method(self, execute_methods, real_suffix)
@@ -146,6 +196,40 @@ def patch_queryset_for_measurement() -> QueryStats:
         _patch_cls_replace_method(cls, execute_methods, real_suffix, _execute)
         _patch_cls_replace_method(cls, make_query_methods, real_suffix, _make_query)
 
+    for cls in client_clss:
+        for method in client_methods:
+            async def _execute(self: BaseDBAsyncClient, *args, __original_method=method, **kwargs) -> Any:
+                depth = 3 + _using_executor_ctx.get() * 2
+                name, execute_real = _get_patched_cls_original_method(self, (__original_method,), real_suffix)
+                with measure_time(f"{self.__class__.__name__}.{name}()", add_depth=depth) as _time_spent:
+                    result = await execute_real(*args, **kwargs)
+
+                query_stats = handler_stats_ctx.get(None)
+                if query_stats is not None:
+                    query_stats.client_count += 1
+                    query_stats.client_time += _time_spent.ms
+
+                return result
+
+            _execute.__name__ = f"_{cls.__name__}_{method}"
+            _patch_cls_replace_method(cls, (method,), real_suffix, _execute)
+
+    async def _PoolConnectionWrapper___aenter(self: PoolConnectionWrapper, *args, **kwargs):
+        depth = 6 + _using_executor_ctx.get() * 2
+        name, execute_real = _get_patched_cls_original_method(self, aenter_methods, real_suffix)
+        with measure_time(f"{self.__class__.__name__}.{name}()", add_depth=depth) as _time_spent:
+            return await execute_real(*args, **kwargs)
+
+    _patch_cls_replace_method(PoolConnectionWrapper, aenter_methods, real_suffix, _PoolConnectionWrapper___aenter)
+
+    async def _PoolConnectionWrapper___aexit(self: PoolConnectionWrapper, *args, **kwargs):
+        depth = 6 + _using_executor_ctx.get() * 2
+        name, execute_real = _get_patched_cls_original_method(self, aexit_methods, real_suffix)
+        with measure_time(f"{self.__class__.__name__}.{name}()", add_depth=depth) as _time_spent:
+            return await execute_real(*args, **kwargs)
+
+    _patch_cls_replace_method(PoolConnectionWrapper, aexit_methods, real_suffix, _PoolConnectionWrapper___aexit)
+
     return query_stats_all
 
 
@@ -154,5 +238,12 @@ def unpatch_queryset_for_measurement() -> None:
         _unpatch_cls_replaced_method(cls, execute_methods, real_suffix)
         _unpatch_cls_replaced_method(cls, make_query_methods, real_suffix)
 
+    for cls in client_clss:
+        for method in client_methods:
+            _unpatch_cls_replaced_method(cls, (method,), real_suffix)
+
+    _unpatch_cls_replaced_method(BaseExecutor, executor_select_methods, real_suffix)
     _unpatch_cls_replaced_method(RequestHandler, call_methods, real_suffix)
     _unpatch_cls_replaced_method(Session, resolve_ctx_methods, real_suffix)
+    _unpatch_cls_replaced_method(PoolConnectionWrapper, aenter_methods, real_suffix)
+    _unpatch_cls_replaced_method(PoolConnectionWrapper, aexit_methods, real_suffix)
